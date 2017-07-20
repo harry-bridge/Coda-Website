97444dd4ba550c7afcc830330e77a2fecab7e65e
Harry Bridge <harry@harrybridge.co.uk>
2017-07-20 14:24:34 +0100

    Remove random chars from hugo script

--- _bin/hugo.sh
+++ _bin/hugo.sh
@@ -3,4 +3,4 @@
 mkdir ~/hugo && cd "$_"
 curl -Lk https://github.com/spf13/hugo/releases/download/v0.18.1/hugo_0.18.1_Linux-64bit.tar.gz | tar xz
 mv hugo*/hugo* ~/hugo018 && chmod +x "$_"
-exit 0cd
+exit
